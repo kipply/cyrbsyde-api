0f6b5ff6812d353b1d3fc630f72d279d14803cfa
Sergey Smirnov <sergey@smirnov.nyc>
2017-09-10 01:59:09 -0400

    adding config file because lol

---
+++ config.py
@@ -0,0 +1,5 @@
+class Config:
+    LYFT_AUTH_HEADER = 'Bearer 7qpfp4aezbRHX0KnwZvB8w1/9lwqHB2fzcBEZcOIMB6gWZyuQLGx4LEPAf1hscurLEd0xVOx8uvLxh7EkLejdUPr/HlJrlrjVQ2Q+FmqLOuMuceFjKMgTO4='
+    LYFT_CLIENT_ID = 'fPqQjXzeLfin'
+    LYFT_CLIENT_TOKEN = '7qpfp4aezbRHX0KnwZvB8w1/9lwqHB2fzcBEZcOIMB6gWZyuQLGx4LEPAf1hscurLEd0xVOx8uvLxh7EkLejdUPr/HlJrlrjVQ2Q+FmqLOuMuceFjKMgTO4='
+    LYFT_CLIENT_SECRET = '8X-XVVj5KuW1Kw3KsJU_hWazM7qKRT44'
